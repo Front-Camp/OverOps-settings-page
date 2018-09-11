d7b9fa5a272b7ed817d69bede8c1813fc5d7e163
Volodymyr Shevchuk <dosandkv@gmail.com>
2018-09-12 01:55:20 +0300

    Breadcrumbs: page-proofs #7

--- src/components/breadcrumbs/index.jsx
+++ src/components/breadcrumbs/index.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import styles from './breadcrumbs.scss';
 
-const Breadcrumbs = () => {
-  // TODO: we need build it dynamically
+const Breadcrumbs = ({match}) => {
+  const items = match.url.split('/').filter(item => item);
 
   return (
     <div className={styles.breadcrumbs}>
-      <div>
-        <Link to="/settings/environments">General Settings</Link>
-      </div>
-      <div>
-        <Link to="/settings/environment/Dev">Environment Settings</Link>
-      </div>
-      {/*&gt; Environments*/}
+      {
+        items.map((item, index) => {
+          return (
+            <span className={styles.item} key={index}>
+              <Link to="/settings">{item}</Link>
+              {
+                index < items.length - 1
+                  ? <span>&nbsp; &gt; &nbsp;</span>
+                  : null
+              }
+            </span>
+          );
+        })
+      }
     </div>
   );
 };
